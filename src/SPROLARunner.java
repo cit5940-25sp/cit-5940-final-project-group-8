d862407c9050e260f1089c50c6626cfd338c965e
Chengrong Yu <chengry@seas.upenn.edu>
2025-05-10 23:05:38 -0400

    Update SPROLARunner.java
    Add comments

--- src/SPROLARunner.java
+++ src/SPROLARunner.java
@@ -11,14 +11,16 @@ public class SPROLARunner {
             return;
         }
 
-
+        // Read the contents of the source file into a single string
         String code = Files.readString(Paths.get(args[0]));
 
-
+        // Create a Lexer to tokenize the input source code
         Lexer lexer = new Lexer(code);
+        
+        // Create a Parser to build the AST (Abstract Syntax Tree) from tokens
         Parser parser = new Parser(lexer);
 
-
+        // Initialize a function table to store user-defined functions
         HashMap<String, UserFunction> functionTable = new HashMap<>();
         Environment globalEnv = new Environment(null, functionTable);
 
